4bbdf897deeea3b90d35271f8a33adde03410c47
mna712 <mennaelgharabawii@gmail.com>
2025-09-02 18:30:43 +0300

    pull chamges

--- middlewares/asyncWrapper.js
+++ middlewares/asyncWrapper.js
@@ -1,12 +1,10 @@
-// const asyncWrapper = (asyncFn) => {
-//   return (req, res, next) => {
-//     asyncFn(req, res, next).catch((err) => {
-//       next(err);
-//     });
-//   };
-// };
-
-const asyncWrapper = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+const asyncWrapper = (asyncFn) => {
+  return (req, res, next) => {
+    asyncFn(req, res, next).catch((err) => {
+      next(err);
+    });
+  };
 };
+
 export default asyncWrapper;
+
