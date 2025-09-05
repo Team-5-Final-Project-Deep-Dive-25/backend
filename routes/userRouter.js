a41bc841c777e765c8725fcfe82148675ffbc9d4
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-09-05 18:56:33 +0300

    feat

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -16,7 +16,6 @@ import { getProfile } from "../controllers/users/getProfile.js";
 import updateProfile from "../controllers/users/updateProfile.js";
 import { normalizeProductImages } from "../controllers/validation/product.js";
 import { verifyEmail } from "../controllers/users/verifyEmail.js";
-import { resend } from "../controllers/users/resend.js";
 import multer from "multer";
 const userRouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -65,6 +64,5 @@ userRouter.delete(
 userRouter.delete("/", protect, asyncWrapper(deleteAccount));
 
 userRouter.get("/verify", asyncWrapper(verifyEmail));
-userRouter.get("/resend", asyncWrapper(resend));
 
 export default userRouter;
