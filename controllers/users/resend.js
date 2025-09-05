40bbf247431b8be6e039ac0b16ec0e8fa9555ee8
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-09-05 19:16:51 +0300

    feat: add resend file

---
+++ controllers/users/resend.js
@@ -0,0 +1,26 @@
+import { User } from "../../models/userModel.js";
+import { SUCCESS, FAIL } from "../../utilities/successWords.js";
+
+import crypto from "crypto";
+import { sendVerificationEmail } from "../../middlewares/emailVerification.js";
+
+export const resend = async (req, res) => {
+  const { email } = req.body;
+  const userVerify = await User.findOne({ email });
+  if (userVerify.isVerified === true) {
+    return res.status(400).json({
+      status: 400,
+      success: FAIL,
+      message: "This user is already verifyed",
+    });
+  }
+  const verificationTokenz = crypto.randomBytes(32).toString("hex");
+  userVerify.verificationToken = verificationTokenz;
+  await userVerify.save();
+  await sendVerificationEmail(email, verificationTokenz);
+  return res.status(200).json({
+    status: 200,
+    success: SUCCESS,
+    message: "the email has been resended ",
+  });
+};
