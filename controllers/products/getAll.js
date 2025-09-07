97acbd9b01fe9a37e34bbea9b4310f75b799a1e8
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-09-07 13:54:06 +0300

    personal

--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -1,56 +1,67 @@
-import { Product } from "../../models/productModel.js";
-import { FAIL, SUCCESS } from "../../utilities/successWords.js";
+// import { Product } from "../../models/productModel.js";
+// import { FAIL, SUCCESS } from "../../utilities/successWords.js";
 
-export const getAll = async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+// export const getAll = async (req, res) => {
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = parseInt(req.query.limit) || 10;
+//   const skip = (page - 1) * limit;
 
-  // Build filter object
-  const filter = { deleted_at: null };
-  if (req.query.category) {
-    filter.categoryId = req.query.category;
+//   // Build filter object
+//   const filter = { deleted_at: null };
+//   if (req.query.category) {
+//     filter.categoryId = req.query.category;
+//   }
+
+//   const products = await Product.find(filter, {
+//     __v: 0,
+//     createdAt: 0,
+//     updatedAt: 0,
+//     deleted_at: 0,
+//   })
+//     .populate({
+//       path: "categoryId",
+//       select: "-__v -updatedAt -createdAt -deleted_at",
+//       populate: {
+//         path: "discountId",
+//         select: "-__v -updatedAt -createdAt -deleted_at -categoryId -productId",
+//       },
+//     })
+
+//     .populate({
+//       path: "discountId",
+//       select: "-productId -categoryId -__v -createdAt -updatedAt -deleted_at",
+//     })
+//     .skip(skip)
+//     .limit(limit);
+//   if (products.length === 0) {
+//     return res.status(404).json({
+//       success: FAIL,
+//       status: 404,
+//       message: "No Products found",
+//     });
+//   }
+//   const total = await Product.countDocuments(filter);
+
+//   return res.status(200).json({
+//     success: SUCCESS,
+//     status: 200,
+//     message: "Products fetched successfully",
+//     data: products,
+//     total,
+//     page,
+//     pages: Math.ceil(total / limit),
+//   });
+// };
+
+// export default getAll;
+import fetch from "node-fetch";
+
+export const fetchData = async (req, res) => {
+  try {
+    const response = await fetch("https://clutch.co/eg/hr/outsourcing");
+    const data = await response.text(); // use .json() if endpoint returns JSON
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-
-  const products = await Product.find(filter, {
-    __v: 0,
-    createdAt: 0,
-    updatedAt: 0,
-    deleted_at: 0,
-  })
-    .populate({
-      path: "categoryId",
-      select: "-__v -updatedAt -createdAt -deleted_at",
-      populate: {
-        path: "discountId",
-        select: "-__v -updatedAt -createdAt -deleted_at -categoryId -productId",
-      },
-    })
-
-    .populate({
-      path: "discountId",
-      select: "-productId -categoryId -__v -createdAt -updatedAt -deleted_at",
-    })
-    .skip(skip)
-    .limit(limit);
-  if (products.length === 0) {
-    return res.status(404).json({
-      success: FAIL,
-      status: 404,
-      message: "No Products found",
-    });
-  }
-  const total = await Product.countDocuments(filter);
-
-  return res.status(200).json({
-    success: SUCCESS,
-    status: 200,
-    message: "Products fetched successfully",
-    data: products,
-    total,
-    page,
-    pages: Math.ceil(total / limit),
-  });
 };
-
-export default getAll;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,53 +1,41 @@
-// import jwt from "jsonwebtoken";
-// import { FAIL } from "../utilities/successWords.js";
-// import { Black } from "../models/blacklist.js";
+import jwt from "jsonwebtoken";
+import { FAIL } from "../utilities/successWords.js";
+import { Black } from "../models/blacklist.js";
 
-// export const protect = async (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-//   const tokean = authHeader.startsWith("Bearer ")
-//     ? authHeader.slice(7)
-//     : authHeader;
-//   console.log(tokean);
-//   const tokenexists = await Black.findOne({ invalid: tokean });
-//   console.log(tokenexists);
-//   if (tokenexists) {
-//     return res.status(401).json({
-//       status: 401,
-//       success: FAIL,
-//       message: "Your token is not vaild any more",
-//     });
-//   }
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     return res.status(401).json({
-//       status: 401,
-//       success: FAIL,
-//       message: "Not authorized, token missing",
-//     });
-//   }
+export const protect = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const tokean = authHeader.startsWith("Bearer ")
+    ? authHeader.slice(7)
+    : authHeader;
+  console.log(tokean);
+  const tokenexists = await Black.findOne({ invalid: tokean });
+  console.log(tokenexists);
+  if (tokenexists) {
+    return res.status(401).json({
+      status: 401,
+      success: FAIL,
+      message: "Your token is not vaild any more",
+    });
+  }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      status: 401,
+      success: FAIL,
+      message: "Not authorized, token missing",
+    });
+  }
 
-//   const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
 
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       status: 401,
-//       success: FAIL,
-//       message: "Not authorized, token invalid " + error.message,
-//     });
-//   }
-// };
-// controllers/fetchController.js
-import fetch from "node-fetch";
-
-export const fetchData = async (req, res) => {
   try {
-    const response = await fetch("https://clutch.co/eg/hr/outsourcing");
-    const data = await response.text(); // use .json() if endpoint returns JSON
-    res.status(200).send(data);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res.status(401).json({
+      status: 401,
+      success: FAIL,
+      message: "Not authorized, token invalid " + error.message,
+    });
   }
 };
