5be613a6b19078158e9e43b1d436c9a37a346460
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-11-23 11:14:13 +0200

    fix: import axios

--- controllers/webhook/postReceive.js
+++ controllers/webhook/postReceive.js
@@ -1,51 +1,52 @@
 import dotenv from "dotenv";
+import axios from "axios";
 dotenv.config();
 
+const token = process.env.WHATSAPP_TOKEN;
+
 const postReceive = async (req, res) => {
-  let body_param = req.body;
+  const body = req.body;
+  console.log(JSON.stringify(body, null, 2));
 
-  console.log(JSON.stringify(body_param, null, 2));
+  try {
+    if (!body?.object) return res.sendStatus(404);
 
-  if (body_param.object) {
-    console.log("inside body param");
-    if (
-      body_param.entry &&
-      body_param.entry[0].changes &&
-      body_param.entry[0].changes[0].value.messages &&
-      body_param.entry[0].changes[0].value.messages[0]
-    ) {
-      let phon_no_id =
-        body_param.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+    const entry = body.entry?.[0];
+    const changes = entry?.changes?.[0];
+    const value = changes?.value;
+    const message = value?.messages?.[0];
 
-      console.log("phone number " + phon_no_id);
-      console.log("from " + from);
-      console.log("boady param " + msg_body);
+    if (!message) return res.sendStatus(404);
 
-      axios({
-        method: "POST",
-        url:
-          "https://graph.facebook.com/v24.0/" +
-          phon_no_id +
-          "/messages?access_token=" +
-          token,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: {
-            body: "Hi.. I'm Prasath, your message is " + msg_body,
-          },
+    const phoneNumberId = value.metadata?.phone_number_id;
+    const from = message.from;
+
+    // Handle only text messages
+    const msg_body = message.text?.body || "";
+
+    console.log("phone number", phoneNumberId);
+    console.log("from", from);
+    console.log("body", msg_body);
+
+    await axios.post(
+      `https://graph.facebook.com/v24.0/${phoneNumberId}/messages?access_token=${token}`,
+      {
+        messaging_product: "whatsapp",
+        to: from,
+        text: {
+          body: `Hi.. I'm Khaled, your message is: ${msg_body}`,
         },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      },
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
 
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error("Error:", error.response?.data || error.message);
+    return res.sendStatus(500);
   }
 };
+
 export default postReceive;
